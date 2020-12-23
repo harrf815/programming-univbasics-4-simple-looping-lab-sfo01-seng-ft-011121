91afdfe23de141a277bf938b882df6a2493adda0
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-23 23:43:55 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,5 +6,33 @@ def loop_message_five_times(string)
     count += 1
   end
 end 
+ 
+def loop_message_n_times(string, number)
+  count = 0
+  
+  until count == number do
+    puts string
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0 
+  
+  while count < array.length do
+    puts array[count]
+    count += 1 
+  end
+end
+
+def return_string_array(array)
+  count = 0 
+  
+  while count < array.length do 
+    array[count].to_s 
+    count += 1 
+  end 
+end
+  
   
   
